27e8f97d7e63caf169b6dd479e6c082efed7bd73
arinhalicki <arinhalicki@yahoo.com>
2018-03-05 11:56:57 -0800

    Updated Socket functionality

--- server.js
+++ server.js
@@ -4,25 +4,27 @@ var interact = require("interactjs");
 var app = express();
 
 app.use(express.static(path.join(__dirname, "/static")));
-app.set('views', path.join(__dirname, "/views"));
-app.set('view engine', 'ejs');
-app.get('/', function (req, res) {
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
+app.get("/", function (req, res) {
     res.render("index.ejs");
 });
 
-var server = app.listen(8000, function() {
+var server = app.listen(8000, function () {
     console.log("listening on port 8000");
 });
 
-var io = require('socket.io').listen(server);
+var io = require("socket.io").listen(server);
 
-io.sockets.on('connection', function (socket) {
+io.sockets.on("connection", function (socket) {
     console.log("Client/socket is connected!");
     console.log("Client/socket id is: ", socket.id);
     //all the server socket code goes in here
 
-    socket.on('tile_clicked', function (data) {
-        console.log('Someone dragged a tile -> X: ' + data.x + ", Y: " + data.y);
-        socket.emit('server_response', { response: "sockets are the best!" });
-    })
-})
+    socket.on("tile_clicked", function (data) {
+        console.log("Someone dragged a tile -> X: " + data.x + ", Y: " + data.y);
+        socket.broadcast.emit("server_response", {
+            response: data
+        });
+    });
+});
